07cd95b88dce106616a27d0664e804ca67ad4f74
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-02-19 21:42:38 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Inter } from "@next/font/google";
+import BenefitsSection from "components/BenefitsSection";
+import CardSection from "components/CardSection";
+import { benefitsData } from "components/data";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <main></main>;
+}
